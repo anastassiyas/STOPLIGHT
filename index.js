a792a7905e927737c3fccfc46b4de7d1cb7f1147
anastassiya.savostina <a.savostina@icloud.com>
2022-01-23 23:30:09 -0700

    done

--- index.js
+++ index.js
@@ -7,51 +7,43 @@
     var go = document.getElementById("goButton")
   
     stop.addEventListener("click", function () {
-    document.getElementById('stopLight').classList.toggle('stop')
-    })
-  
-  
-    slow.addEventListener("click", function () {
-    document.getElementById('slowLight').classList.toggle('slow')
-    })
-  
-  
-    go.addEventListener("click", function () {
-    document.getElementById('goLight').classList.toggle('go')
-    })
-  
-    //Add new DOM event listeners and handlers to log the mouse state of each button.
-    stop.addEventListener('mouseenter', e => {
-    console.log("Entered stop button");
-    });
-  
-    stop.addEventListener('mouseleave', e => {
-    console.log("Left stop button");
-  
-    });
-  
-    slow.addEventListener('mouseenter', e => {
-    console.log("Entered slow button");
-    });
-  
-    slow.addEventListener('mouseleave', e => {
-    console.log("Left slow button");
-  
-    });
-  
-    go.addEventListener('mouseenter', e => {
-    console.log("Entered go button");
-    });
-  
-    go.addEventListener('mouseleave', e => {
-    console.log("Left go button");
-  
-    });
-  
-  
-  })
-  
-    ();
-  
-  
-  
+        document.getElementById('stopLight').classList.toggle('stop')
+        })
+            
+        slow.addEventListener("click", function () {
+        document.getElementById('slowLight').classList.toggle('slow')
+        })
+           
+        go.addEventListener("click", function () {
+        document.getElementById('goLight').classList.toggle('go')
+        })
+      
+        //Add new DOM event listeners and handlers to log the mouse state of each button.
+        stop.addEventListener('mouseenter', e => {
+        console.log("Entered stop button");
+        });
+      
+        stop.addEventListener('mouseleave', e => {
+        console.log("Left stop button");
+        });
+      
+        slow.addEventListener('mouseenter', e => {
+        console.log("Entered slow button");
+        });
+      
+        slow.addEventListener('mouseleave', e => {
+        console.log("Left slow button");
+        });
+      
+        go.addEventListener('mouseenter', e => {
+        console.log("Entered go button");
+        });
+      
+        go.addEventListener('mouseleave', e => {
+        console.log("Left go button");
+        });
+   
+      }) ();
+      
+      
+      
